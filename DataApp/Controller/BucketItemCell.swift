34b661abef77b63226b3390e8d546c025d07267e
Ethan M <guitarworthy365@gmail.com>
2018-11-30 08:21:57 -0700

    Finished layout, started on bucket item.

---
+++ DataApp/Controller/BucketItemCell.swift
@@ -0,0 +1,24 @@
+//
+//  BucketItemCellTableViewCell.swift
+//  DataApp
+//
+//  Created by Mumford, Ethan on 11/30/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+import UIKit
+
+class BucketItemCellTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
